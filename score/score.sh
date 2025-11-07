2a4b2f2d346ebcd54c917cc891c67c36122c1214
csxsl555 <csxsi@sjtu.edu.cn>
2025-11-07 22:12:24 +0800

    fix: fix bugs of main.cpp.

--- score/score.sh
+++ score/score.sh
@@ -42,8 +42,8 @@ EOF
     echo ""
 done
 
-L_EXTRA=10
-R_EXTRA=10
+L_EXTRA=1
+R_EXTRA=20
 for ((i = $L_EXTRA; i <= $R_EXTRA; i = i + 1))
 do
     echo ""
--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,44 @@
 #include "Def.hpp"
-#include "syntax.hpp"
-#include "expr.hpp"
-#include "value.hpp"
 #include "RE.hpp"
-#include <sstream>
-#include <iostream>
+#include "expr.hpp"
+#include "syntax.hpp"
+#include "value.hpp"
 #include <cassert>
+#include <iostream>
+#include <limits>
 #include <map>
+#include <sstream>
 
 extern std::map<std::string, ExprType> primitives;
 extern std::map<std::string, ExprType> reserved_words;
 
 bool isExplicitVoidCall(Expr expr) {
-    MakeVoid* make_void_expr = dynamic_cast<MakeVoid*>(expr.get());
+    MakeVoid *make_void_expr = dynamic_cast<MakeVoid *>(expr.get());
     if (make_void_expr != nullptr) {
         return true;
     }
-    
-    Apply* apply_expr = dynamic_cast<Apply*>(expr.get());
+
+    Apply *apply_expr = dynamic_cast<Apply *>(expr.get());
     if (apply_expr != nullptr) {
-        Var* var_expr = dynamic_cast<Var*>(apply_expr->rator.get());
+        Var *var_expr = dynamic_cast<Var *>(apply_expr->rator.get());
         if (var_expr != nullptr && var_expr->x == "void") {
             return true;
         }
     }
-    
-    Begin* begin_expr = dynamic_cast<Begin*>(expr.get());
+
+    Begin *begin_expr = dynamic_cast<Begin *>(expr.get());
     if (begin_expr != nullptr && !begin_expr->es.empty()) {
         return isExplicitVoidCall(begin_expr->es.back());
     }
-    
-    If* if_expr = dynamic_cast<If*>(expr.get());
+
+    If *if_expr = dynamic_cast<If *>(expr.get());
     if (if_expr != nullptr) {
         return isExplicitVoidCall(if_expr->conseq) || isExplicitVoidCall(if_expr->alter);
     }
-    
-    Cond* cond_expr = dynamic_cast<Cond*>(expr.get());
+
+    Cond *cond_expr = dynamic_cast<Cond *>(expr.get());
     if (cond_expr != nullptr) {
-        for (const auto& clause : cond_expr->clauses) {
+        for (const auto &clause : cond_expr->clauses) {
             if (clause.size() > 1 && isExplicitVoidCall(clause.back())) {
                 return true;
             }
@@ -46,29 +47,30 @@ bool isExplicitVoidCall(Expr expr) {
     return false;
 }
 
-void REPL(){
+void REPL() {
     // read - evaluation - print loop
     Assoc global_env = empty();
-    while (1){
-        #ifndef ONLINE_JUDGE
-            std::cout << "scm> ";
-        #endif
-        Syntax stx = readSyntax(std :: cin); // read
-        try{
-            Expr expr = stx -> parse(global_env); // parse
-            // stx -> show(std :: cout); // syntax print
-            
-            Value val = expr -> eval(global_env);
-            if (val -> v_type == V_TERMINATE)
+    while (1) {
+#ifndef ONLINE_JUDGE
+        std::cout << "scm> ";
+#endif
+        Syntax stx = readSyntax(std ::cin); // read
+        try {
+            Expr expr = stx->parse(global_env); // parse
+            Value val = expr->eval(global_env);
+            if (val->v_type == V_TERMINATE)
                 break;
-            if (val -> v_type != V_VOID) {
-                val -> show(std :: cout); // value print (skip void values)
+            bool is_void_value = (val->v_type == V_VOID);
+            bool is_explicit_void = isExplicitVoidCall(expr);
+            if (!is_void_value || is_explicit_void) {
+                val->show(std::cout); // value print
+                // puts("");
             }
-            //assert(0);
-        }
-        catch (const RuntimeError &RE){
-            //std :: cout << RE.message();
-            std :: cout << "RuntimeError";
+        } catch (const RuntimeError &RE) {
+            std::cout << "RuntimeError";
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            // puts("");
         }
         puts("");
     }
